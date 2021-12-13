9d23d3256de07f1859cf71229d1f881cf2c80c28
keitey <keiteybarros@gmail.com>
2021-12-13 18:04:03 -0300

    commit inicial

---
+++ desafio-3/index.js
@@ -0,0 +1,7 @@
+let res = confirm ("Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?")
+
+if (res == true){
+    alert("Ótimo! Nós temos as melhores camasde toda a região!")
+}else{
+    alert("Que pena! Você parecia seruma pessoa mais legal")
+}
---
+++ desafio-6/index.js
@@ -0,0 +1,27 @@
+function soma(numero1, numero2) {
+    let soma = numero1 + numero2
+    return console.log ('A soma dos dois números é: ' + soma)
+    }
+
+var soma = soma(8, 2)
+
+function divisao(numero1, numero2) {
+    let divisao = numero1 / numero2
+    return console.log ("A divisão dos dois números é: " + divisao)
+}
+
+var divisao = divisao (8, 4)
+
+function multiplicacao(numero1, numero2){
+    let multiplicacao = numero1 * numero2 
+    return console.log("A multiplicação dos dois números é:" + multiplicacao)
+}
+
+var multiplicacao = multiplicacao(2, 3)
+
+function subtracao(numero1, numero2){
+    let subtracao = numero1 - numero2 
+    return console.log("A subtração dos dois números é:" + subtracao)
+}
+
+var subtracao = subtracao (3,2)
